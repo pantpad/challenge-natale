74c168b7f295d86f5e0b8d36debf6f8580b75ba5
pantpad <138708449+pantpad@users.noreply.github.com>
2023-12-28 13:15:37 +0100

    Insert test modal to visually represent planet

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
 import { useState } from "react";
 import "./App.css";
 
+import earth from "./assets/images/rotating_earth.gif";
+
 function App() {
+  const dialog = document.getElementById("dialog");
+  console.log(dialog);
+  function openDialog() {
+    let bodyWidth = document.body.clientWidth;
+    document.body.style.overflow = "hidden";
+    document.body.style.width = `${bodyWidth}`;
+    dialog.showModal();
+  }
+
+  function closeDialog() {
+    document.body.style.overflow = "auto";
+    document.body.style.width = "auto";
+    dialog.close();
+  }
+
   return (
     <div className="bodyContainer">
       <div className="titleContainer">
@@ -10,7 +27,21 @@ function App() {
       <div className="planetContainer">
         <h2>Seleziona un pianeta!</h2>
         <div className="planetGrid">
-          <div className="planet"></div>
+          <div className="planet">
+            <button onClick={openDialog}>
+              <img src={earth} alt="" />
+            </button>
+            <dialog id="dialog">
+              <div>
+                <h2>Planet Title</h2>
+                <p>Planet Description</p>
+                <p>Planet Distance from the sun</p>
+                <p>Planet Diameter</p>
+                <p>Planet Gravity</p>
+              </div>
+              <button onClick={closeDialog}>Close Planet</button>
+            </dialog>
+          </div>
           <div className="planet"></div>
           <div className="planet"></div>
           <div className="planet"></div>
