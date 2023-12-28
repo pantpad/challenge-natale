eaa3f69c15d026e24ae6fa56060a06a530872f0f
pantpad <138708449+pantpad@users.noreply.github.com>
2023-12-28 17:36:04 +0100

    Add dialog close on backdrop click

--- src/App.jsx
+++ src/App.jsx
@@ -6,28 +6,13 @@ import Planet from "./Planet/Planet";
 import planetsData from "./database/planets";
 
 function App() {
-  const dialog = document.getElementById("dialog");
-  console.log(dialog);
-  function openDialog() {
-    //let bodyWidth = document.body.clientWidth;
-    //document.body.style.overflow = "hidden";
-    //document.body.style.width = `${bodyWidth}`;
-    dialog.showModal();
-  }
-
-  function closeDialog() {
-    //document.body.style.overflow = "auto";
-    //document.body.style.width = "auto";
-    dialog.close();
-  }
-
   return (
     <div className="bodyContainer">
       <div className="titleContainer">
         <h1>REACT PLANETS</h1>
       </div>
       <div className="planetContainer">
-        <h2>Seleziona un pianeta!</h2>
+        <h2>Select a Planet! </h2>
         <div className="planetGrid">
           <Planet {...planetsData[0]} />
           <Planet {...planetsData[1]} />
--- src/Planet/Planet.jsx
+++ src/Planet/Planet.jsx
@@ -5,16 +5,22 @@ function Planet({ name, image, title, description, distanceFromSun, diameter, gr
 
   function openDialog(e) {
     dialog = e.target.parentElement.nextSibling;
-    //let bodyWidth = document.body.clientWidth;
-    //document.body.style.overflow = "hidden";
-    //document.body.style.width = `${bodyWidth}`;
+    let bodyWidth = document.body.clientWidth;
+    document.body.style.overflow = "hidden";
+    document.body.style.width = `${bodyWidth}`;
     dialog.showModal();
   }
 
   function closeDialog() {
-    //document.body.style.overflow = "auto";
-    //document.body.style.width = "auto";
+    document.body.style.overflow = "auto";
+    document.body.style.width = "auto";
     dialog.close();
+  }
+
+  function handleBackdropClick(e) {
+    if (e.target == dialog) {
+      dialog.close();
+    }
   }
 
   return (
@@ -23,15 +29,17 @@ function Planet({ name, image, title, description, distanceFromSun, diameter, gr
       <button onClick={openDialog}>
         <img src={image} alt={title} />
       </button>
-      <dialog id="dialog">
-        <div>
-          <h2>{name.toUpperCase()}</h2>
-          <p>{description}</p>
-          <p>Distance from the sun: {distanceFromSun}</p>
-          <p>Planet Diameter: {diameter}</p>
-          <p>Planet Gravity: {gravity}</p>
+      <dialog id="dialog" onClick={handleBackdropClick}>
+        <div className="dialogContent">
+          <div>
+            <h2>{name.toUpperCase()}</h2>
+            <p>{description}</p>
+            <p>Distance from the sun: {distanceFromSun}</p>
+            <p>Planet Diameter: {diameter}</p>
+            <p>Planet Gravity: {gravity}</p>
+          </div>
+          <button onClick={closeDialog}>Close Planet</button>
         </div>
-        <button onClick={closeDialog}>Close Planet</button>
       </dialog>
     </div>
   );
